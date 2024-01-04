b18a108905bcfbd1d735c6ee63ead7271f8e499e
jilcimar <jilcimar.fernandes0267@gmail.com>
2024-01-04 17:17:03 -0300

    feat: :rocket: servidor grpc, funcionando

--- server/main.go
+++ server/main.go
@@ -1,7 +1,34 @@
-package server
+package main
 
-import "hello/grpc/pb"
+import (
+	"context"
+	"google.golang.org/grpc"
+	"hello/grpc/pb"
+	"log"
+	"net"
+)
 
 type Server struct {
 	pb.UnimplementedHelloServer
+}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Ola, " + in.GetName()}, nil
+}
+
+func main() {
+	println("Rodando o servidor gRPC")
+
+	listener, err := net.Listen("tcp", "localhost:9000")
+
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &Server{})
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("Falha no servidor: %v", err)
+	}
 }
